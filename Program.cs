8381f5039a8e4f1e04416fcadeb4dc33bab118c2
Sue Xiong <sxiong0519@gmail.com>
2021-09-08 13:11:24 -0500

    created a add member method

--- Program.cs
+++ Program.cs
@@ -19,7 +19,7 @@ namespace HeistII_Oceans5
                 new Muscle {Name = "Bane", SkillLevel = 40, PercentageCut = 33}
             };
 
-            public void CreateMember()
+             void CreateMember()
             {
                 Console.WriteLine($"{rolodex.Count}");
                 Console.WriteLine("Name of the new crew member:");
@@ -29,22 +29,22 @@ namespace HeistII_Oceans5
                 List<string> specialty = new List<string>{
                     "Hacker", "Lock Specialist", "Muscle"
                 };
-                Console.Writeline("Enter crew member's skill level between 1 and 100.");
-                int memberSkillLevel = int.Parse(Console.Readline());
-                Console.WriteLine("Enter requested percentage cut.")
-                int memberPercentageCut = int.Parse(Console.Readline());
+                Console.WriteLine("Enter crew member's skill level between 1 and 100.");
+                int memberSkillLevel = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter requested percentage cut.");
+                int memberPercentageCut = int.Parse(Console.ReadLine());
 
                 if (memberSpecialty == 1)
                 {
-                    rolodex.Add(new Hacker {Name = memberName, SkillLevel = member, PercentageCut = memberPercentageCut});
+                    rolodex.Add(new Hacker {Name = memberName, SkillLevel = memberSkillLevel, PercentageCut = memberPercentageCut});
                 } else if (memberSpecialty == 2)
                 {
-                    rolodex.Add(new LockSpecialist {Name = memberName, SkillLevel = member, PercentageCut = memberPercentageCut});
+                    rolodex.Add(new LockSpecialist {Name = memberName, SkillLevel = memberSkillLevel, PercentageCut = memberPercentageCut});
                 } else
                 {
-                    rolodex.Add(new Muscle {Name = memberName, SkillLevel = member, PercentageCut = memberPercentageCut});
-                }
+                    rolodex.Add(new Muscle {Name = memberName, SkillLevel = memberSkillLevel, PercentageCut = memberPercentageCut});
             }
+}
 
 
         }

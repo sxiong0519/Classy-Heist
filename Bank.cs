bc2ec6cc09f28e6ad6c04bc418dc08f9c0ae0c53
hcrudge <hcrudge1@gmail.com>
2021-09-08 14:42:55 -0500

    updated rolodex & recon

--- Bank.cs
+++ Bank.cs
@@ -8,11 +8,58 @@ namespace HeistII_Oceans5
         public int AlarmScore { get; set; }
         public int VaultScore { get; set; }
         public int SecurityGuardScore { get; set; }
-        public bool IsSecure {
+        public bool IsSecure
+        {
             get
             {
-                return AlarmScore + VaultScore + SecurityGuardScore > 0; 
+                return AlarmScore + VaultScore + SecurityGuardScore > 0;
             }
+        }
+
+        public string MostSecure
+        {
+            get
+            {
+                if (AlarmScore > VaultScore && AlarmScore > SecurityGuardScore)
+                {
+                    return "Alarm";
+                }
+                else if (VaultScore > AlarmScore && VaultScore > SecurityGuardScore)
+                {
+                    return "Vault";
+                }
+                else
+                {
+                    return "Security Guard";
+                }
+
+            }
+
+        }
+        public string LeastSecure
+        {
+            get
+            {
+                if (AlarmScore < VaultScore && AlarmScore < SecurityGuardScore)
+                {
+                    return "Alarm";
+                }
+                else if (VaultScore < AlarmScore && VaultScore < SecurityGuardScore)
+                {
+                    return "Vault";
+                }
+                else
+                {
+                    return "Security Guard";
+                }
+
+            }
+        }
+        public void ReconReport()
+        {
+        Console.WriteLine($"Most Secure: {MostSecure}");
+        Console.WriteLine($"Least Secure: {LeastSecure}");
+        
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -7,6 +7,17 @@ namespace HeistII_Oceans5
     {
         static void Main(string[] args)
         {
+            Bank newBank = new Bank
+            {
+               AlarmScore = new Random().Next(0,101),
+               VaultScore = new Random().Next(0,101),
+               SecurityGuardScore = new Random().Next(0,101),
+               CashOnHand = new Random().Next(50000, 1000001)
+            };
+            // Console.WriteLine($"{newBank.MostSecure} & {newBank.LeastSecure} & {newBank.AlarmScore} & {newBank.VaultScore} & {newBank.SecurityGuardScore} & {newBank.CashOnHand} & {newBank.IsSecure}");
+            newBank.ReconReport();
+
+        
             List<IRobber> rolodex = new List<IRobber>{
                 new Hacker {Name = "Fingers", SkillLevel = 20, PercentageCut = 10},
                 new LockSpecialist {Name = "Lockin Lakin", SkillLevel = 75, PercentageCut = 20},
@@ -18,34 +29,43 @@ namespace HeistII_Oceans5
                 new LockSpecialist {Name = "Randy", SkillLevel = 30, PercentageCut = 15},
                 new Muscle {Name = "Bane", SkillLevel = 40, PercentageCut = 33}
             };
-
-             void CreateMember()
+            CreateMember();
+            void CreateMember()
             {
                 Console.WriteLine($"{rolodex.Count}");
                 Console.WriteLine("Name of the new crew member:");
                 string memberName = Console.ReadLine();
-                Console.WriteLine("What is their specialty? 1)Hacker 2)Lock Specialist 3)Muscle");
-                int memberSpecialty = int.Parse(Console.ReadLine());
-                List<string> specialty = new List<string>{
+                if (memberName != "")
+                {
+                    Console.WriteLine("What is their specialty? 1)Hacker 2)Lock Specialist 3)Muscle");
+                    int memberSpecialty = int.Parse(Console.ReadLine());
+                    List<string> specialty = new List<string>{
                     "Hacker", "Lock Specialist", "Muscle"
                 };
-                Console.WriteLine("Enter crew member's skill level between 1 and 100.");
-                int memberSkillLevel = int.Parse(Console.ReadLine());
-                Console.WriteLine("Enter requested percentage cut.");
-                int memberPercentageCut = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter crew member's skill level between 1 and 100.");
+                    int memberSkillLevel = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter requested percentage cut.");
+                    int memberPercentageCut = int.Parse(Console.ReadLine());
 
-                if (memberSpecialty == 1)
-                {
-                    rolodex.Add(new Hacker {Name = memberName, SkillLevel = memberSkillLevel, PercentageCut = memberPercentageCut});
-                } else if (memberSpecialty == 2)
-                {
-                    rolodex.Add(new LockSpecialist {Name = memberName, SkillLevel = memberSkillLevel, PercentageCut = memberPercentageCut});
-                } else
-                {
-                    rolodex.Add(new Muscle {Name = memberName, SkillLevel = memberSkillLevel, PercentageCut = memberPercentageCut});
+                    if (memberSpecialty == 1)
+                    {
+                        rolodex.Add(new Hacker { Name = memberName, SkillLevel = memberSkillLevel, PercentageCut = memberPercentageCut });
+                    }
+                    else if (memberSpecialty == 2)
+                    {
+                        rolodex.Add(new LockSpecialist { Name = memberName, SkillLevel = memberSkillLevel, PercentageCut = memberPercentageCut });
+                    }
+                    else
+                    {
+                        rolodex.Add(new Muscle { Name = memberName, SkillLevel = memberSkillLevel, PercentageCut = memberPercentageCut });
+                    }
+                    CreateMember();
+                }
+
             }
-}
 
+            
+                
 
         }
     }

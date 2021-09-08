398a2034d3997d5012fcc6098f7e15a9c95d804a
hcrudge <hcrudge1@gmail.com>
2021-09-08 09:29:09 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HeistII_Oceans5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
